c396d1d3830279c206175edc518a0f18db7ca2b3
t0ottio13 <adsoption104@gmail.com>
2021-06-19 22:20:39 +0900

    ログインと投稿機能追加

---
+++ posts/post_input.php
@@ -0,0 +1,37 @@
+<?php
+  include('../functions.php');
+  session_start();
+  // check_session_id();
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>投稿作成</title>
+</head>
+
+<body>
+  <form action="post_create.php" method="POST">
+      <a href="post_read.php">一覧画面</a>
+      <a href="post_logout.php">logout</a>
+      <div>
+        post_text: <input type="text" name="post_text">
+      </div>
+      <!-- あとで追加します。by hashi -->
+      <!-- <div>
+        post_img: <input type="file" name="post_img">
+      </div> -->
+      <!-- <div>
+        post_place: <input type="hidden" name="post_place">
+      </div> -->
+      <div>
+        <button>submit</button>
+      </div>
+  </form>
+
+</body>
+
+</html>
---
+++ users_login_act.php
@@ -0,0 +1,37 @@
+<?php
+
+
+session_start();
+include('./functions.php');
+$pdo = connect_to_db();
+$user_name=$_POST['user_name'];
+$password=$_POST['password'];
+
+
+$sql = 'SELECT * FROM users_table
+                WHERE user_name = :user_name
+                AND   password = :password
+                AND   is_deleted = 0';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindValue(':user_name', $user_name, PDO::PARAM_STR);
+$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+$status = $stmt->execute();
+
+$val = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// var_dump(!$val);
+// exit();
+if(!$val){
+    echo('ログイン情報に誤りがあります');
+    echo ('<a href="todo_login.php">login</a>');
+    exit();
+}else{
+    $_SESSION['session_id']= session_id();
+    $_SESSION['is_admin']= $val['is_admin'];
+    $_SESSION['user_name']= $val['user_name'];
+    // URL長めに入れないと通らない
+    // 各自自分のに合わせて
+    header("Location: /G's/team_dev_php/posts/post_read.php");
+    exit();
+}
