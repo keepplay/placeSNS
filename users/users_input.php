b2483a548e00687aea3b2b1e8dad12fef293a53f
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-06-20 16:15:31 +0900

    06201615

--- users/users_input.php
+++ users/users_input.php
@@ -4,26 +4,39 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/kasho.css">
+
   <title>ユーザー管理画面（入力画面）</title>
 </head>
 
 <body>
-  <form action="users_create.php" method="POST">
-      <p>一覧リンクはあとで消す</p>
-      <a href="users_read.php">一覧画面</a>
-      <div>
-        username: <input type="text" name="user_name">
+  <section class="form_parent">
+
+    <form class="form" action="users_create.php" method="POST">
+      <h1 class="title">新規会員登録</h1>
+
+      <!-- 会員登録 -->
+      <div class="firstbox">
+        <div>
+          <input type="text" class="username" name="username" placeholder="ユーザー名">
+        </div>
+        <div>
+          <input type="email" class="useremail" name="email" placeholder="shima@example.com">
+        </div>
+        <div>
+          <input type="text" class="password" name="password" placeholder="パスワード">
+        </div>
+        <div>
+          <button class="action_btn" id="touroku_btn">登録する</button>
+        </div>
       </div>
-      <div>
-        email: <input type="email" name="email">
+      <div class="secondbox">
+        <p class="newperson_text">アカウントをお持ちの方はこちら</p>
+
+        <button class="action_btn" id="login_gamen_btn" onclick="loction.href='../users_login.php'">ログイン画面へ</a>
+
       </div>
-      <div>
-        password: <input type="password" name="password">
-      </div>
-      <div>
-        <button>submit</button>
-      </div>
-  </form>
+    </form>
 
 </body>
 
