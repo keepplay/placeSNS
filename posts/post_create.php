0710a751d6e0ec6398fcdc3a9d3688cbf2a4e6b6
t0ottio13 <adsoption104@gmail.com>
2021-06-23 17:24:19 +0900

    位置情報が投稿に追加されないバグ修正

--- posts/post_create.php
+++ posts/post_create.php
@@ -1,6 +1,6 @@
 <?php
 // var_dump($_POST);
-// exit();
+
 // var_dump($_FILES);
 // exit();
 include("../functions.php");
--- posts/post_input.php
+++ posts/post_input.php
@@ -92,19 +92,19 @@ session_start();
 
     </div>
 
-    <!-- あとで追加します。by hashi -->
+    <!-- あとで追加します。by hashi
     <div>
       post_place: <input type="hidden" name="post_place">
-    </div>
+    </div> -->
 
     </form>
 
 
   </div>
-  <!-- <section class="map_area">
+  <section class="map_area">
     <p>位置情報取得&Map表示</p>
     <div id="map"></div>
-  </section> -->
+  </section>
 
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -112,9 +112,62 @@ session_start();
 
   <script src='https://www.bing.com/api/maps/mapcontrol?key=AjFcJuWxXYNwMC0PS8_ZQb8m92xQad3BpO3K9s7XJVoAoGuHv0eQIe4Vr5wIjQaN' async defer></script>
   <!-- 位置情報を取得するJS -->
-  <script src="./get_location_info.js"></script>
+  <script>
+   // map表示用に使用する変数
+    let map;
 
+    const option = {
+        enableHighAccuracy: true,
+        maximumAge: 20000,
+        timeout: 1000000,
+    };
+
+    // エラーがでた時の処理
+    function showError(error) {
+        let e = '';
+        if (error.code == 1) {
+        e = '位置情報が許可されてません';
+        } else if (error.code == 2) {
+            e = '現在位置を特定できません';
+        }else if (error.code == 3) {
+        e = '位置情報を取得する前にタイムアウトになりました';
+        }
+        alert('error:' + e);
+    }
+
+    // 地図の表示の関数を定義
+    function mapsInit(position) {
+        const lat = position.coords.latitude.toFixed(4);
+        const lng = position.coords.longitude.toFixed(4);
+        // map = new Microsoft.Maps.Map('#map', {
+        // center: {
+        //     latitude: lat,
+        //     longitude: lng
+        // },
+        // zoom: 20,
+        // });
+        console.log(position);
+        console.log(lat, lng);
+        $("#post_lat").val(lat);
+        $("#post_lng").val(lng);
+
+    }
+
+    // 位置情報の取得
+    function getPosition() {
+        navigator.geolocation
+        .getCurrentPosition(mapsInit, showError, option);
+      // .watchPosition(mapsInit, showError, option);
+    }
+
+    // jsファイルの読み込みが終わってから処理を開始。
+    // 実際に実行しているのはここ！
+    window.onload = function() {
+        getPosition();
+    }
+  </script>
   <script src="../kasho.js"></script>
+  
 
 
 </body>
